a316e4fa8285a3e88ef1fd5263195324cbf3a6cc
Sql Practice <sqlpractice99@gmail.com>
2025-04-08 09:23:24 +0300

    Server <> Added Arabic Title, Sub_title, and Content for the Attributes in Tables Data and Tenders.

---
+++ server/database/migrations/2025_04_08_060746_add_english_title_sub_title_to_tenders_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tenders', function (Blueprint $table) {
+            $table->text('enTitle')->after('title');
+            $table->text('enSub_title')->after('sub_title');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tenders', function (Blueprint $table) {
+            $table->dropColumn(['enTitle', 'enSub_title']);
+        });
+    }
+};
